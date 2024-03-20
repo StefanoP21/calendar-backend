cd3bf5b2322ca1228550e67d808a91bc5f5fde08
Stefano <stefanop21@outlook.es>
2024-03-20 18:16:30 -0500

    refactor(controllers/auth.js): Middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,7 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
 
 const createUser = (req, res = response) => {
   const { name, email, password } = req.body;
-
-  // Express validator
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped(),
-    });
-  }
 
   res.status(201).json({
     ok: true,
@@ -25,16 +14,6 @@ const createUser = (req, res = response) => {
 
 const loginUser = (req, res = response) => {
   const { name, email } = req.body;
-
-  // Express validator
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped(),
-    });
-  }
 
   res.status(200).json({
     ok: true,
@@ -56,5 +35,3 @@ module.exports = {
   loginUser,
   renewToken,
 };
-
-// Path: routes/auth.js
