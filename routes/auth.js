a55337eb2368cc61bff7b32a71e9f8b77a48076a
Stefano <stefanop21@outlook.es>
2024-03-20 17:51:00 -0500

    update(routes/auth.js): Express validator

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,36 @@
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
 const router = Router();
 
 const { createUser, loginUser, renewToken } = require('../controllers/auth');
 
-router.post('/new', createUser);
+router.post(
+  '/new',
+  [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El correo es obligatorio').isEmail(),
+    check(
+      'password',
+      'La contraseña debe tener al menos 6 caracteres'
+    ).isLength({ min: 6 }),
+  ],
+  createUser
+);
 
-router.post('/', loginUser);
+router.post(
+  '/',
+  [
+    check('email', 'El correo es obligatorio').isEmail(),
+    check('password', 'La contraseña es obligatoria').not().isEmpty(),
+    check(
+      'password',
+      'La contraseña debe tener al menos 6 caracteres'
+    ).isLength({ min: 6 }),
+  ],
+  loginUser
+);
 
 router.get('/renew', renewToken);
 
