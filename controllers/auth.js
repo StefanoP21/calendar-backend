ea8f04eec30ce8fcdf34de92edede8120e511977
Stefano <stefanop21@outlook.es>
2024-03-20 17:51:50 -0500

    update(controllers/auth.js): Manejo de errores

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,20 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 const createUser = (req, res = response) => {
   const { name, email, password } = req.body;
 
-  res.json({
+  // Express validator
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped(),
+    });
+  }
+
+  res.status(201).json({
     ok: true,
     msg: 'POST - Register',
     name,
@@ -15,7 +26,17 @@ const createUser = (req, res = response) => {
 const loginUser = (req, res = response) => {
   const { name, email } = req.body;
 
-  res.json({
+  // Express validator
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped(),
+    });
+  }
+
+  res.status(200).json({
     ok: true,
     msg: 'POST - Login',
     name,
