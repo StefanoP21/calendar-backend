e008990480a5d3372deea56c4242734f2383736e
Stefano <stefanop21@outlook.es>
2024-03-19 19:43:24 -0500

    config(index.js): Express

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello world!');
+const express = require('express');
+
+// Express Server
+const app = express();
+
+// Routes
+app.get('/', (req, res) => {
+  res.json({ ok: true, message: 'Hello World' });
+});
+
+// Server
+app.listen(4000, () => {
+  console.log(`Server is running on port ${4000}`);
+});
