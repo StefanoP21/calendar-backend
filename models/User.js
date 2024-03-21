153bdfb58fe8ae79383286630447dba99286819c
Stefano <stefanop21@outlook.es>
2024-03-21 10:45:09 -0500

    new(models/User.js): Esquema de usuario

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = Schema({
+  name: {
+    type: String,
+    required: [true, 'The name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'The email is required'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'The password is required'],
+  },
+});
+
+module.exports = model('User', UserSchema);
