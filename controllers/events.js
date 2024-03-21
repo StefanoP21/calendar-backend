369e890a88dd0ffd4c925f8090d0527269268006
Stefano <stefanop21@outlook.es>
2024-03-21 17:49:00 -0500

    update(controllers/events.js): Guardar evento en la base de datos

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const Event = require('../models/Event');
 
 const getEvents = (req, res = response) => {
   res.status(200).json({
@@ -7,11 +8,25 @@ const getEvents = (req, res = response) => {
   });
 };
 
-const createEvent = (req, res = response) => {
-  res.status(200).json({
-    ok: true,
-    msg: 'POST - createEvent',
-  });
+const createEvent = async (req, res = response) => {
+  const event = new Event(req.body);
+
+  try {
+    event.user = req.uid;
+
+    const eventDb = await event.save();
+
+    res.status(200).json({
+      ok: true,
+      event: eventDb,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Talk to the administrator',
+    });
+  }
 };
 
 const updateEvent = (req, res = response) => {
