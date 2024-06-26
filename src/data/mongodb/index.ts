68193a334b375d6e48c5995e61d5a6a73e554e80
Stefano <stefanop21@outlook.es>
2024-06-25 21:11:17 -0500

    user - model

--- src/data/mongodb/index.ts
+++ src/data/mongodb/index.ts
@@ -1,2 +1,3 @@
 export { MongoDatabase } from './init';
 export { EventModel } from './models/event.model';
+export { UserModel } from './models/user.model';
---
+++ src/data/mongodb/models/user.model.ts
@@ -0,0 +1,19 @@
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'The name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'The email is required'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'The password is required'],
+  },
+});
+
+export const UserModel = model('User', userSchema, 'users');
