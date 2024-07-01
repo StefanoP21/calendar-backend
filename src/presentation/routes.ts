65f874a18e4ccb36e37fa1289311a7638fe49a85
Stefano <stefanop21@outlook.es>
2024-07-01 00:02:16 -0500

    update: routes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { EventRoutes } from './event/routes';
+import { AuthRoutes } from './auth/routes';
 
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
 
     router.use('/api/events', EventRoutes.routes);
+    router.use('api/auth', AuthRoutes.routes);
 
     return router;
   }
